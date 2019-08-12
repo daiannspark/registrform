fff1e097235cd71406756072b9df6c769863616c
Diana Markevich <44816231+daiannspark@users.noreply.github.com>
2019-08-12 16:03:03 +0300

    form

---
+++ src/Action/userAction.jsx
@@ -0,0 +1,3 @@
+const registerUser = user => {};
+
+export default registerUser;
---
+++ src/Components/InputGroup.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const InputGroup = props => {
+  const { name, value, isError, labelName, inputType, eventHandler } = props;
+  return (
+    <div className='form-group'>
+      <label htmlFor={labelName.replace(" ", "_").toLowerCase()}>
+        {labelName}
+      </label>
+      <input
+        id={labelName.replace(" ", "_").toLowerCase()}
+        type={inputType}
+        name={name}
+        className={`form-control ${isError ? "is-invalid" : null}`}
+        placeholder={labelName}
+        onChange={eventHandler}
+        defaultValue={value}
+      />
+    </div>
+  );
+};
+
+InputGroup.propTypes = {
+  labelName: PropTypes.string.isRequired,
+  eventHandler: PropTypes.func.isRequired,
+  inputType: PropTypes.string
+};
+
+InputGroup.defaultProps = {
+  inputType: "text"
+};
+
+export default InputGroup;
