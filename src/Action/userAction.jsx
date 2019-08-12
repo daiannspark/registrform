f92f4bb2511bdd5751298a9efcff5938477572e7
Diana Markevich <44816231+daiannspark@users.noreply.github.com>
2019-08-12 16:27:03 +0300

    user action

--- src/Action/userAction.jsx
+++ src/Action/userAction.jsx
@@ -1,3 +1,10 @@
-const registerUser = user => {};
+import axios from "axios";
+
+const registerUser = async user => {
+    const res = await axios.post("/user/", user);
+    if(res.headers && res.headers.status === 200) {
+        console.log("success");
+    }
+};
 
 export default registerUser;
